a9582e54b94f3186982a50bcad02192c73386488
Vinicius <harchierhygino@gmail.com>
2024-02-26 12:04:09 -0300

    adicionando os exercicios 03 a 05

---
+++ estrutura_sequencial/exercicio_03.py
@@ -0,0 +1,5 @@
+#03-Faça um Programa que peça dois números e imprima a soma.
+
+x = int(input("Digite um número: "))
+y = int(input("Digite outro número: "))
+print(f"Soma = {x + y}")
---
+++ estrutura_sequencial/exercicio_05.py
@@ -0,0 +1,5 @@
+#05-Faça um Programa que converta metros para centímetros.
+
+metros = float(input("Digite a quantidade de metros: "))
+centimetros = metros * 100
+print(f"A quantidade em cm é: {centimetros} cm")
